bd6030c7439e5470b00432c33dfe0692c2ec36d8
guigui55 <guigui55@outlook.jp>
2024-09-16 06:51:57 +0900

    240916 0651

---
+++ ByteTest/ByteTest/Module1.vb
@@ -0,0 +1,29 @@
+﻿Imports System.Diagnostics
+Imports System
+
+Module Module1
+
+    Sub Main()
+        Dim upperByte As Byte = 1 ' 上位バイト
+        Dim lowerByte As Byte = 10 ' 下位バイト
+        Dim result As Integer
+
+        ' 16ビットの数値として組み立てる
+        result = upperByte * 256 + lowerByte
+
+        Console.WriteLine(result) ' 結果は 266
+
+        '###
+        ' バイト配列を初期化し、最初の値に100を代入
+        Dim byteArray As Byte() = New Byte() {100, 50, 200}
+        Dim buf As Double
+
+        ' byteArray(0) の値に 256 を掛ける
+        buf = byteArray(0) * 256.0#
+        Console.WriteLine(buf)
+
+        Console.WriteLine("Program Done. Press Any Key.")
+        Console.ReadKey()
+    End Sub
+
+End Module
---
+++ DataGridViewAndCheckBox/DataGridViewAndCheckBox/FormDatagridViewAndCheckBox.Designer.vb
@@ -0,0 +1,95 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormDatagridViewAndCheckBox
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.No = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Check = New System.Windows.Forms.DataGridViewCheckBoxColumn()
+        Me.Value1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Value2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.No, Me.Check, Me.Value1, Me.Value2})
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 12)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowTemplate.Height = 21
+        Me.DataGridView1.Size = New System.Drawing.Size(522, 150)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(372, 169)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'No
+        '
+        Me.No.HeaderText = "No"
+        Me.No.Name = "No"
+        '
+        'Check
+        '
+        Me.Check.HeaderText = "Check"
+        Me.Check.Name = "Check"
+        Me.Check.Resizable = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.Check.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic
+        '
+        'Value1
+        '
+        Me.Value1.HeaderText = "Value1"
+        Me.Value1.Name = "Value1"
+        '
+        'Value2
+        '
+        Me.Value2.HeaderText = "Value2"
+        Me.Value2.Name = "Value2"
+        '
+        'FormDatagridViewAndCheckBox
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(595, 204)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Name = "FormDatagridViewAndCheckBox"
+        Me.Text = "DatagiridView And CheckBox"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Button1 As Button
+    Friend WithEvents No As DataGridViewTextBoxColumn
+    Friend WithEvents Check As DataGridViewCheckBoxColumn
+    Friend WithEvents Value1 As DataGridViewTextBoxColumn
+    Friend WithEvents Value2 As DataGridViewTextBoxColumn
+End Class
