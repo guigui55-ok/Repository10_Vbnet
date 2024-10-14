b6c99edf22c1426fe20b830d77da42103cd1064c
guigui55 <guigui55@outlook.jp>
2024-10-15 00:36:43 +0900

    241015 0036

--- VbUtility/CommonUtilityPrj/Module1.vb
+++ VbUtility/CommonUtilityPrj/Module1.vb
@@ -1,7 +1,38 @@
 ﻿Module Module1
 
     Sub Main()
-
     End Sub
+
+
+    Function GetEnumArray(enumType As Type)
+        If enumType.IsEnum Then
+            Return [Enum].GetNames(enumType)
+        Else
+            Throw New ArgumentException("指定された型はEnumではありません。")
+        End If
+        Return 0
+    End Function
+
+
+    Sub MainEnum()
+        Dim enumCount As Integer = [Enum].GetValues(GetType(TestEnum)).Length
+        Console.WriteLine("Enum の値の個数: " & enumCount)
+
+        Dim EnumAry As String() = GetEnumArray(GetType(TestEnum))
+        For Each buf In EnumAry
+            Console.WriteLine(buf.GetType().ToString())
+            Console.WriteLine(buf)
+        Next
+        'Console.WriteLine("Enum 値: " & String.Join(", ", New List(Of String)().AddRange(EnumAry)))
+        Console.WriteLine("Enum 値: " & String.Join(", ", EnumAry))
+
+        Console.ReadKey()
+    End Sub
+
+    Enum TestEnum
+        DEFAULT_VAL = 0
+        VALUE_A = 1
+        VALUE_B = 2
+    End Enum
 
 End Module
